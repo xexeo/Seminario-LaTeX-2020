cfe3cc955f2aad2a47fab62dcd6a4891f598984c
Geraldo Xexéo <xexeo@cos.ufrj.br>
2020-05-14 16:58:36 -0300

    Entrando em Fatos e Mitos

---
+++ Images/Linguagens de Marcação.mm
@@ -0,0 +1,51 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1589481894717" ID="ID_555612926" MODIFIED="1589482061707" TEXT="Linguagens de Marca&#xe7;&#xe3;o">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1589481989647" ID="ID_1856503143" MODIFIED="1589482061686" POSITION="right" TEXT="De apresenta&#xe7;&#xe3;o">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1589482018432" ID="ID_1162414715" MODIFIED="1589482061689" TEXT="Wikis">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1589481996016" ID="ID_1679410281" MODIFIED="1589482061692" POSITION="right" TEXT="Procedurais">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1589482024672" ID="ID_1291599730" MODIFIED="1589482061694" TEXT="TeX">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1589482037176" ID="ID_1946140085" MODIFIED="1589482061696" TEXT="LaTeX">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1589482026431" ID="ID_948395535" MODIFIED="1589482061700" TEXT="PostScript">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1589482031480" ID="ID_228368615" MODIFIED="1589482061702" TEXT="Lout">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1589482000376" ID="ID_61131569" MODIFIED="1589482061703" POSITION="right" TEXT="Descritivas">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1589482044608" ID="ID_1657978692" MODIFIED="1589482061705" TEXT="HTML">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1589482049096" ID="ID_225792591" MODIFIED="1589482061706" TEXT="SGML">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1589482051536" ID="ID_990803947" MODIFIED="1589482061707" TEXT="XML">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
